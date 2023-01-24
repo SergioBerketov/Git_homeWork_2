3574c3794da2e72a823b1ed52c9687873de6301a
Sergei Berketov <sergio_berketov@mail.ru>
2023-01-24 19:15:29 +0700

    added branch 'stat'

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SalesManager sm = new SalesManager(new int[]{3, 6, 9});
         System.out.println(sm.max());
+
+
     }
 }
