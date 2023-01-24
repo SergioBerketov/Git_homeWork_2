8fdc9f19daff51f77eb9d8e1c6ed54d77bf3bf10
Sergei Berketov <sergio_berketov@mail.ru>
2023-01-24 19:27:36 +0700

    added branch 'stat' with checking

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager sm = new SalesManager(new int[]{3, 6, 9});
+        SalesManager sm = new SalesManager(new int[]{3, 6, 9, 20, 106});
         System.out.println(sm.max());
 
+        Statistic sta = new Statistic();
+        sta.arithmeticMean(new int[]{3, 6, 9, 20, 106});
 
     }
 }
